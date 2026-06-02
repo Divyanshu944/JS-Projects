e1816ce99df696da863bf78cafec37d081ed624e
Divyanshu pal <paldivyanshu944@gmail.com>
2026-06-02 15:34:19 -0400

    js

--- Ai-Image_generator/script.js
+++ Ai-Image_generator/script.js
@@ -1,1 +1,18 @@
-const themeToggle = document.querySelector()
+const themeToggle = document.querySelector(".them-logo");
+
+(()=>{
+const saveTheme = localStorage.getItem("theme");
+const systemPrefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+const isDarkTheme = saveTheme === "dark" || (!saveTheme && systemPrefersDark)
+document.body.classList.toggle("dark-theme", isDarkTheme);
+themeToggle.querySelector("i").className = isDarkTheme ? "fa-solid fa-sun":"fa-solid fa-moon"
+})();
+const toggleTheme = () =>{
+    const isDarkTheme = document.body.classList.toggle("dark-theme");
+    localStorage.setItem("theme", isDarkTheme ? "dark" : "light");
+    themeToggle.querySelector("i").className = isDarkTheme ? "fa-solid fa-sun":"fa-solid fa-moon"
+}
+
+themeToggle.addEventListener("click", toggleTheme)
+
